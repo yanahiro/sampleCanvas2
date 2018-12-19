51d69854515b78df968a8b198105714e1cf879d7
yanahiro <yanatch1982@gmail.com>
2018-12-19 18:49:43 +0900

    一部修正
    machine毎の判定が必要になった場合の処理分岐を追加

--- js/settings.js
+++ js/settings.js
@@ -15,14 +15,24 @@ var getStyleSheetValue = (function(elem, prop) {
 
 var bWidth = 0;
 var setCanvasSize = (function() {
+  // Style 
   var csObj = document.querySelector('#cs');
   var width = getStyleSheetValue(csObj, 'width');
   var height = getStyleSheetValue(csObj, 'height');
 
-  console.log('width : ' + width);
-  console.log('height : ' + height);
+  // window
+  // var wWidth = window.parent.screen.width;
+  // var wHeight = window.parent.screen.height;
+  // // 縦も横も768以上の場合
+  // if (wWidth > 767 && wHeight > 767) {
+  //   // スマートフォン以外と判定
+  // } else {
+  //   // スマートフォンと判定
+  // }
+  // console.log('width : ' + width);
+  // console.log('height : ' + height);
 
-  // 幅(Width)が変わった場合はリサイズする
+  // 初回設定または、幅(Width)が変わった場合はリサイズする
   if (bWidth == 0 || bWidth != width) {
     var elem = document.getElementById("cs");
     elem.setAttribute('width', width);
